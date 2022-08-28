66f7b11fe47d899208c983645c8cd3857fe7534d
mrpurpleknight <ziogaghi99@gmail.com>
2022-08-28 02:04:44 +0200

    removed pointy castle and now using Random.secure

--- lib/src/rng/secure_random_generator.dart
+++ lib/src/rng/secure_random_generator.dart
@@ -2,14 +2,21 @@ import 'dart:math';
 
 class SecureRandomGenerator {
   static BigInt generateDhPrivateValue(int bitLength) {
-    Random rnd = Random.secure();
+    Random? rnd;
+
+    try {
+      rnd = Random.secure();
+    } on UnsupportedError {
+      throw UnsupportedPlatformException(
+          'This platform cannot provide a cryptographically secure source of random numbers');
+    }
 
     BigInt lowerBound = BigInt.two.pow(bitLength - 1);
     late BigInt generated;
 
     bool loopCondition = true;
     while (loopCondition) {
-      generated = BigInt.from(rnd.nextInt(bitLength));
+      generated = BigInt.from(rnd!.nextInt(bitLength));
       if (generated.compareTo(lowerBound) >= 0 &&
           generated.compareTo(BigInt.two * lowerBound) < 0) {
         loopCondition = false;
@@ -18,4 +25,10 @@ class SecureRandomGenerator {
 
     return generated;
   }
+}
+
+class UnsupportedPlatformException {
+  final String message;
+
+  UnsupportedPlatformException(this.message);
 }

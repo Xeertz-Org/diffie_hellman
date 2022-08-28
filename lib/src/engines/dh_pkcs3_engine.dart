6e5712a1729ba0fd984a0386ea5fa919518869ae
mrpurpleknight <ziogaghi99@gmail.com>
2022-08-28 02:01:18 +0200

    removed pointy castle and now using Random.secure

--- lib/src/engines/dh_pkcs3_engine.dart
+++ lib/src/engines/dh_pkcs3_engine.dart
@@ -2,14 +2,13 @@ import 'package:diffie_hellman/src/dh_groups.dart';
 import 'package:diffie_hellman/src/engines/dh_engine.dart';
 import 'package:diffie_hellman/src/models/dh_key_pair.dart';
 import 'package:diffie_hellman/src/models/dh_parameter_spec.dart';
-import 'package:diffie_hellman/src/rng/dh_fortuna_prng.dart';
+import 'package:diffie_hellman/src/rng/secure_random_generator.dart';
 import 'package:flutter/cupertino.dart';
 
 /// Concrete implementation of [DhEngine].
 /// Use the constructor DhPkcs3Engine.fromGroup() with a specific group ID
 /// for predefined engine objects based on https://www.ietf.org/rfc/rfc3526.txt
 class DhPkcs3Engine implements DhEngine {
-
   final DhParameterSpec _parameterSpec;
 
   late final BigInt _publicKey;
@@ -69,7 +68,7 @@ class DhPkcs3Engine implements DhEngine {
   @override
   @protected
   BigInt generatePrivateKey() {
-    return DhFortunaPrng.generateDhPrivateValue(_parameterSpec.length);
+    return SecureRandomGenerator.generateDhPrivateValue(_parameterSpec.length);
   }
 
   @override
--- lib/src/rng/secure_random_generator.dart
+++ lib/src/rng/secure_random_generator.dart
@@ -1,21 +1,15 @@
-import 'package:pointycastle/pointycastle.dart';
-import 'package:pointycastle/src/platform_check/platform_check.dart';
+import 'dart:math';
 
-class DhFortunaPrng {
+class SecureRandomGenerator {
   static BigInt generateDhPrivateValue(int bitLength) {
-    SecureRandom rnd = SecureRandom('Fortuna')
-      ..seed(
-        KeyParameter(
-          Platform.instance.platformEntropySource().getBytes(32),
-        ),
-      );
+    Random rnd = Random.secure();
 
     BigInt lowerBound = BigInt.two.pow(bitLength - 1);
     late BigInt generated;
 
     bool loopCondition = true;
     while (loopCondition) {
-      generated = rnd.nextBigInteger(bitLength);
+      generated = BigInt.from(rnd.nextInt(bitLength));
       if (generated.compareTo(lowerBound) >= 0 &&
           generated.compareTo(BigInt.two * lowerBound) < 0) {
         loopCondition = false;
